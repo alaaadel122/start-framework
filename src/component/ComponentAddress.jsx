f3cad83b16f0d1016c1e755312a4336f1047f152
alaaadel122 <alaaadel109@gmail.com>
2025-07-22 20:31:10 +0300

    sec

--- src/component/ComponentAddress.jsx
+++ src/component/ComponentAddress.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export default function ComponentAddress({ componentName, color }) {
   return (
-    <div className='text-white text-center pt-5  text-uppercase'>
+    <div className='text-white text-center pt-5 mt-5 text-uppercase'>
       <h2 className='comp-address' style={{ color: color }}>{componentName}</h2>
     </div>
   )
